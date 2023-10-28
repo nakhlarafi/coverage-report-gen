032846c15f887f39bf5b65c5ca9c5bbdfe355f81
nakhlarafi <nakhla054@gmail.com>
2023-10-28 01:34:19 -0400

    branch feature added, but can not correctly detect coverage for some cases

--- src/main/java/math/InvokeStaticInstrumenter.java
+++ src/main/java/math/InvokeStaticInstrumenter.java
@@ -7,23 +7,26 @@ import soot.util.*;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.*;
 
 public class InvokeStaticInstrumenter extends BodyTransformer {
-
+	private static boolean initialized = false;
 	static SootClass counterClass;
-	static SootMethod markExecuted, reportCounter;
+	static SootMethod markExecuted, reportCounter, markBranch;
 
 	static {
 		counterClass = Scene.v().loadClassAndSupport("math.StatementCoverageCounter");
 		markExecuted = counterClass.getMethod("void markExecuted(java.lang.String,java.lang.String)");
 		reportCounter = counterClass.getMethod("void report()");
+		markBranch = counterClass.getMethod("void markBranch(java.lang.String,java.lang.String,boolean)");
 	}
 
 	@Override
 	protected void internalTransform(Body body, String phase, Map options) {
+		initialize();
 		SootMethod method = body.getMethod();
 		String methodName = method.getSignature();
 
@@ -32,58 +35,96 @@ public class InvokeStaticInstrumenter extends BodyTransformer {
 		Chain<Unit> units = body.getUnits();
 		Iterator<Unit> stmtIt = units.snapshotIterator();
 
-		int totalStatements = 0; // Variable to store total number of statements for the method
+		int totalStatements = 0;
 		int sequenceNumber = 0;
+		int totalBranches = 0;
+
 		while (stmtIt.hasNext()) {
 			Stmt stmt = (Stmt) stmtIt.next();
 
-			// Skip parameter assignments based on string content
-//			if (stmt.toString().contains("@parameter")){
-//				System.out.println("----------");
-//				System.out.println(stmt instanceof AssignStmt);
-//				System.out.println(stmt.toString());
-//				System.out.println(stmt.getClass().getName());
-//				System.out.println("----------");
-//			}
-
+			// Skip parameters
 			if (stmt instanceof JIdentityStmt && stmt.toString().contains("@parameter")) {
 				continue;
 			}
 
-//			String stmtId = stmt.toString();
-			String stmtId = stmt.toString() + "_" + sequenceNumber;
-			sequenceNumber++;
+			sequenceNumber = instrumentStatement(units, stmt, methodName, sequenceNumber);
+			totalStatements++;
 
-			// Mark the statement as executed
-			InvokeExpr markExecutedExpr = Jimple.v().newStaticInvokeExpr(markExecuted.makeRef(), StringConstant.v(methodName), StringConstant.v(stmtId));
-			Stmt markExecutedStmt = Jimple.v().newInvokeStmt(markExecutedExpr);
-			units.insertBefore(markExecutedStmt, stmt);
-
-			totalStatements++;  // Increment the statement count
+			// Instrument branches
+			if (stmt instanceof IfStmt) {
+				sequenceNumber = instrumentBranch(units, stmt, methodName, sequenceNumber);
+				totalBranches += 2;  // Increment the branch count
+			}
 		}
 
-		// Writing the total statements count to a file
-		String data = methodName.trim() + ":" + totalStatements + "\n";
+		writeToFile("methodStatementCounts.txt", methodName, totalStatements);
+		writeToFile("methodBranchCounts.txt", methodName, totalBranches);
+	}
 
+	private int instrumentStatement(Chain<Unit> units, Stmt stmt, String methodName, int sequenceNumber) {
+		String stmtId = stmt.toString() + "_" + sequenceNumber;
+		sequenceNumber++;
+
+		InvokeExpr markExecutedExpr = Jimple.v().newStaticInvokeExpr(markExecuted.makeRef(), StringConstant.v(methodName), StringConstant.v(stmtId));
+		Stmt markExecutedStmt = Jimple.v().newInvokeStmt(markExecutedExpr);
+		units.insertBefore(markExecutedStmt, stmt);
+
+		return sequenceNumber;
+	}
+
+	private int instrumentBranch(Chain<Unit> units, Stmt stmt, String methodName, int sequenceNumber) {
+		String branchId = "branch_" + sequenceNumber;
+		sequenceNumber++;
+
+		// True when the branch is taken
+		insertInvokeBefore(units, stmt, methodName, branchId, 1);
+
+		// False when the branch is not taken (else branch)
+		insertInvokeAfter(units, stmt, methodName, branchId, 0);
+
+		return sequenceNumber;
+	}
+
+	private void insertInvokeBefore(Chain<Unit> units, Stmt stmt, String methodName, String id, int value) {
+		InvokeExpr expr = Jimple.v().newStaticInvokeExpr(markBranch.makeRef(), StringConstant.v(methodName), StringConstant.v(id), IntConstant.v(value));
+		Stmt invokeStmt = Jimple.v().newInvokeStmt(expr);
+		units.insertBefore(invokeStmt, stmt);
+	}
+
+	private void insertInvokeAfter(Chain<Unit> units, Stmt stmt, String methodName, String id, int value) {
+		InvokeExpr expr = Jimple.v().newStaticInvokeExpr(markBranch.makeRef(), StringConstant.v(methodName), StringConstant.v(id), IntConstant.v(value));
+		Stmt invokeStmt = Jimple.v().newInvokeStmt(expr);
+		units.insertAfter(invokeStmt, stmt);
+	}
+
+	private void writeToFile(String fileName, String methodName, int value) {
+		String data = methodName.trim() + ":" + value + "\n";
 		try {
-			Files.write(Paths.get("methodStatementCounts.txt"), data.getBytes(), StandardOpenOption.APPEND, StandardOpenOption.CREATE);
+			Files.write(Paths.get(fileName), data.getBytes(), StandardOpenOption.APPEND, StandardOpenOption.CREATE);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
 
-		// Check if it's the main method to insert the report generation code
-		String signature = method.getSubSignature();
-		boolean isMain = signature.equals("void main(java.lang.String[])");
-		if (isMain) {
-			stmtIt = units.snapshotIterator();
-			while (stmtIt.hasNext()) {
-				Stmt stmt = (Stmt) stmtIt.next();
-				if (stmt instanceof ReturnStmt || stmt instanceof ReturnVoidStmt) {
-					InvokeExpr reportExpr = Jimple.v().newStaticInvokeExpr(reportCounter.makeRef());
-					Stmt reportStmt = Jimple.v().newInvokeStmt(reportExpr);
-					units.insertBefore(reportStmt, stmt);
-				}
-			}
+	private synchronized void initialize() {
+		if (!initialized) {
+			// Remove the files if they exist
+			deleteFile("methodStatementCounts.txt");
+			deleteFile("methodBranchCounts.txt");
+
+			initialized = true;
 		}
 	}
+
+	private void deleteFile(String fileName) {
+		Path filePath = Paths.get(fileName);
+		try {
+			if (Files.exists(filePath)) {
+				Files.delete(filePath);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
--- src/main/java/math/StatementCoverageCounter.java
+++ src/main/java/math/StatementCoverageCounter.java
@@ -7,11 +7,35 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StatementCoverageCounter {
     private static Map<String, Integer> coverageMap = new HashMap<>();
     private static Map<String, Integer> methodTotalStmts = new HashMap<>();
     private static Map<String, Integer> methodExecutedStmts = new HashMap<>();
+
+    private static Map<String, Boolean> branchCoverageMap = new HashMap<>();
+    private static Map<String, Integer> methodTotalBranches = new HashMap<>();
+    private static Map<String, Integer> methodTakenBranches = new HashMap<>();
+
+
+    public static synchronized void markBranch(String methodName, String branchId, boolean taken) {
+        String combinedKey = methodName + ":" + branchId + ":" + taken;
+
+        if (!branchCoverageMap.containsKey(combinedKey)) {
+            branchCoverageMap.put(combinedKey, taken);
+            System.out.println(combinedKey);
+            methodTakenBranches.put(methodName, methodTakenBranches.getOrDefault(methodName, 0) + 1);
+
+            // Only update total branches once per branch (since there are two outcomes)
+            if (!branchCoverageMap.containsKey(methodName + ":" + branchId + ":true") &&
+                    !branchCoverageMap.containsKey(methodName + ":" + branchId + ":false")) {
+                methodTotalBranches.put(methodName, methodTotalBranches.getOrDefault(methodName, 0) + 1);
+            }
+        }
+    }
+
+
 
     public static synchronized void markExecuted(String methodName, String stmtId) {
         String combinedKey = methodName + ":" + stmtId;
@@ -24,50 +48,56 @@ public class StatementCoverageCounter {
     }
 
     public static synchronized void report() {
+        System.err.println("Statement Coverage:");
+        printCoverageReport("methodStatementCounts.txt", methodTotalStmts, methodExecutedStmts);
+
+        System.err.println("\nBranch Coverage:");
+        printCoverageReport("methodBranchCounts.txt", methodTotalBranches, methodTakenBranches);
+    }
+
+    private static void printCoverageReport(String fileName, Map<String, Integer> totalMap, Map<String, Integer> executedMap) {
         System.err.println("MethodName\tExecuted/Total\tCoveragePercentage");
         System.err.println("------------------------------------------------------------");
 
-        // Read methodStatementCounts.txt and populate methodTotalStmts map
+        // Populate the total map from the file
         try {
-            List<String> lines = Files.readAllLines(Paths.get("methodStatementCounts.txt"));
+            List<String> lines = Files.readAllLines(Paths.get(fileName));
             for (String line : lines) {
                 line = line.trim();
+                int lastColonIndex = line.lastIndexOf(":");
+                if (lastColonIndex == -1) continue;
 
-                int lastColonIndex = line.lastIndexOf(":"); // Find the position of the last colon
-                if (lastColonIndex == -1) continue; // Skip if no colon is found
+                String methodName = line.substring(0, lastColonIndex).trim();
+                int totalCount = Integer.parseInt(line.substring(lastColonIndex + 1).trim());
 
-                String methodName = line.substring(0, lastColonIndex).trim(); // Extract method name/signature
-                int totalStatements = Integer.parseInt(line.substring(lastColonIndex + 1).trim()); // Extract and parse the count
-
-                methodTotalStmts.put(methodName, totalStatements);
+                totalMap.put(methodName, totalCount);
             }
         } catch (IOException e) {
             e.printStackTrace();
-            System.err.println("Failed to read methodStatementCounts.txt");
+            System.err.println("Failed to read " + fileName);
             return;
         }
 
-        List<String> reportRows = new ArrayList<>();
-
-        // Iterate over methodTotalStmts to gather rows without printing them immediately
-        for (String method : methodTotalStmts.keySet()) {
-            int total = methodTotalStmts.get(method);
-            int executed = methodExecutedStmts.getOrDefault(method, 0);
-            float coverage = ((float) executed / total) * 100;
-            String row = method + "\t" + executed + "/" + total + "\t" + String.format("%.2f", coverage) + "%";
-            reportRows.add(row);
-        }
-
-        // Sort the rows based on coverage percentage
-        reportRows.sort((row1, row2) -> {
+        // Create and sort report rows
+        List<String> reportRows = totalMap.keySet().stream().map(method -> {
+            float coverage;
+            int total = totalMap.get(method);
+            int executed = executedMap.getOrDefault(method, 0);
+            if (total==0 && executed==0){
+                coverage = 0;
+            }
+            else {
+                coverage = ((float) executed / total) * 100;
+            }
+            return method + "\t" + executed + "/" + total + "\t" + String.format("%.2f", coverage) + "%";
+        }).sorted((row1, row2) -> {
             String perc1 = row1.split("\t")[2].replace("%", "");
             String perc2 = row2.split("\t")[2].replace("%", "");
             return Float.compare(Float.parseFloat(perc1), Float.parseFloat(perc2));
-        });
+        }).collect(Collectors.toList());
 
         // Print the sorted rows
-        for (String row : reportRows) {
-            System.err.println(row);
-        }
+        reportRows.forEach(System.err::println);
     }
+
 }

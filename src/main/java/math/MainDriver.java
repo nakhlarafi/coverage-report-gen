643b5ca308b597430047becbc4e2217ec91b350c
nakhlarafi <nakhla054@gmail.com>
2023-10-29 23:10:10 -0400

    argument passing added to run the MainDriver class

--- src/main/java/math/MainDriver.java
+++ src/main/java/math/MainDriver.java
@@ -1,41 +1,51 @@
-package math;/* Usage: java math.MainDriver [soot-options] appClass
- */
+package math;
 
-
-/* import necessary soot packages */
 import soot.*;
 import soot.options.Options;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MainDriver {
     public static void main(String[] args) {
-
-        /* check the arguments */
+        // Check if any arguments were passed
         if (args.length == 0) {
             System.err.println("Usage: java math.MainDriver [options] classname");
             System.exit(0);
         }
 
-//        /*Set the soot-classpath to include the helper class and class to analyze*/
-//        Options.v().set_soot_classpath(Scene.v().defaultClassPath() + ":target/classes:Sample/bin");
-        String defaultPath = Scene.v().defaultClassPath();
-        String helperClassesPath = "target/classes";
-        String classesToInstrumentPath = "/Users/tahminaakter/Desktop/ECSE688/PA2/Profiler/Sample/";
+        // Extract custom arguments
+        List<String> argsList = new ArrayList<>(Arrays.asList(args));
 
+        // Default paths
+        String defaultPath = Scene.v().defaultClassPath();
+        String helperClassesPath = extractAndRemoveArg(argsList, "-helperClassesPath", "target/classes");
+        String classesToInstrumentPath = extractAndRemoveArg(argsList, "-classesToInstrumentPath", "");
+
+        // Set the soot-classpath with the paths provided
         Options.v().set_soot_classpath(defaultPath + ":" + helperClassesPath + ":" + classesToInstrumentPath);
 
-        /* add a phase to transformer pack by call Pack.add */
+        // Add a phase to the transformer pack
         Pack jtp = PackManager.v().getPack("jtp");
-        jtp.add(new Transform("jtp.instrumenter",
-                new InvokeStaticInstrumenter()));
+        jtp.add(new Transform("jtp.instrumenter", new InvokeStaticInstrumenter()));
 
-        /*
-         * Give control to Soot to process all options,
-         * InvokeStatic Instrumenter.internalTransform will get called.
-         */
+        // Pass control to Soot
+        soot.Main.main(argsList.toArray(new String[0]));
+    }
 
-        for(String arg:args){
-            System.out.println(arg);
+    /**
+     * Extracts the value for the given argument key and removes both the key and value from the list.
+     * If the key is not found, returns the default value.
+     */
+    private static String extractAndRemoveArg(List<String> argsList, String argKey, String defaultValue) {
+        int index = argsList.indexOf(argKey);
+        if (index != -1 && argsList.size() > index + 1) {
+            String value = argsList.get(index + 1);
+            argsList.remove(index + 1);  // remove value
+            argsList.remove(index);      // remove key
+            return value;
         }
-        soot.Main.main(args);
+        return defaultValue;
     }
 }
